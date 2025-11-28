9c9bed21e2fb9ed1454b038344bc05622d07f656
sebastian <raynosebastian19@gmail.com>
2025-11-28 10:39:39 +0800

    feat: Introduce full-fledged project management system with Kanban board, authentication, and comprehensive CRUD operations for tasks, projects, and users.

---
+++ proxy.ts
@@ -0,0 +1,33 @@
+import { betterFetch } from "@better-fetch/fetch";
+import type { Session } from "better-auth/types";
+import { NextResponse, type NextRequest } from "next/server";
+
+export default async function proxy(request: NextRequest) {
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        //get the cookie from the request
+        cookie: request.headers.get("cookie") || "",
+      },
+    },
+  );
+
+  const isAuthRoute = request.nextUrl.pathname.startsWith("/login") || request.nextUrl.pathname.startsWith("/signup");
+  const isProtectedRoute = !isAuthRoute && !request.nextUrl.pathname.startsWith("/api") && !request.nextUrl.pathname.startsWith("/_next") && !request.nextUrl.pathname.startsWith("/public");
+
+  if (!session && isProtectedRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (session && isAuthRoute) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
